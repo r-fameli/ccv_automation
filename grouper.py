2a933946d815b5c9d94a38f0016ffbaac19890b8
r-fameli <rikifameli@gmail.com>
2021-05-10 19:11:12 -0400

    Fix issue with Grouper not recognizing one possible member

--- grouper.py
+++ grouper.py
@@ -2,12 +2,14 @@
 
 Used to handle all interactions with Grouper
 """
+import time
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+from webdriver_manager.firefox import GeckoDriverManager
 
 from user_data import ScriptUserCredentials
 
@@ -56,7 +58,7 @@ def add_user_to_grouper(driver: webdriver, your_credentials: ScriptUserCredentia
             break
 
     print("Logged in successfully")
-    
+
     # Search for the user in the Brown:Services:HPC group
     driver.get("https://groups.brown.edu/grouper/populateGroupSummary.do?groupId=a815bed6b1ae442d9d4df08c4f3d61ff")
     driver.find_element_by_xpath("//span[@class='grouperTooltip' and text()='Add members']").click()
@@ -66,16 +68,42 @@ def add_user_to_grouper(driver: webdriver, your_credentials: ScriptUserCredentia
     member_search.send_keys(Keys.RETURN)
 
     # Check that there is only one user
-    grouper_search_results = driver.find_elements_by_xpath("//fieldset/ul/li")
+    # Lite UI: https://groups.brown.edu/grouper/grouperUi/appHtml/grouper.html?operation=Misc.index
+
+    # Sleep momentarily so that everything can load
+    time.sleep(2)
+    grouper_search_results = driver.find_elements_by_xpath("//input[@name='members']")
     if len(grouper_search_results) == 0:
-        print(f"No users found with search term {0}".format(search_term))
+        input("No users found with search term " + search_term + ". Press enter to continue")
         return False
-    elif len(grouper_search_results) > 1:
-        print(f"More than one user found with search term {0}".format(search_term))
-        return False
+    if len(grouper_search_results) > 1:
+        print("Multiple users found with search term" + search_term)
+        input("Select the correct user and press enter")
+        driver.find_element_by_xpath("//input[@class='blueButton' and @value='Assign privileges']").click()
     else:
         driver.find_element_by_xpath("//input[@class='blueButton' and @value='Assign privileges']").click()
 
-    if "successfully assigned" in driver.page_source:
-        print("Successfully added user in Grouper")
-        return True
+    message = driver.find_element_by_xpath("//div[@class='Message']").get_attribute('innerText')
+    print("GROUPER ==> " + message)
+    input("Successfully added user " + search_term + " in Grouper. Press enter to continue")
+    return True
+
+
+# Run this file to test Grouper
+add_user_to_grouper(
+    webdriver.Firefox(executable_path=GeckoDriverManager(cache_valid_range=1).install()),
+    None,
+    "riki_fameli@brown.edu"
+)
+
+
+# NEW UI
+# https://groups.brown.edu/grouper/new
+# Group management page:
+# https://groups.brown.edu/grouper/grouperUi/app/UiV2Main.index?operation=UiV2Group.viewGroup&groupId=a815bed6b1ae442d9d4df08c4f3d61ff
+# Add members page:
+# https://groups.brown.edu/grouper/grouperUi/app/UiV2Main.index?operation=UiV2GroupImport.groupImport&groupId=a815bed6b1ae442d9d4df08c4f3d61ff&backTo=group
+
+# OLD UI
+# Group management page:
+# https://groups.brown.edu/grouper/populateGroupSummary.do?groupId=a815bed6b1ae442d9d4df08c4f3d61ff
